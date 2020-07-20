14efacdd22306cdfa50d0bf20f9bde9221ce7720
zhangjianjun.erin <zhangjianjun.erin@bytedance.com>
2020-07-20 17:48:49 +0800

    server

--- go/src/example/handler.go
+++ go/src/example/handler.go
@@ -1,12 +1,10 @@
 package main
-
 import (
-	"code.byted.org/gopkg/logs"
 	"context"
-	"example/thrift_gen/base"
+	"code.byted.org/gopkg/logs"
 	"example/thrift_gen/webarch/kite/example"
+	"example/thrift_gen/base"
 )
-
 // Implement ExampleMethod Method.
 func ExampleMethod(ctx context.Context, r *example.ExampleReq) (*example.ExampleResp, error) {
 	logs.Infof("ExampleMethod request: %+v", r)
